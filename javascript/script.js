c6474c16762f39410af411b03791e24520475024
Ana Clara <anaclaragamair15@gmail.com>
2026-02-25 19:26:31 -0300

    animação do pipe ok, animação do mario ok, jogo para quando os dois se colidem

---
+++ javascript/script.js
@@ -0,0 +1,35 @@
+//pegando a propiedade de mario
+const mario = document.querySelector('.mario');
+//pegando a propiedade do tubo (pipe)
+const pipe = document.querySelector('.pipe');
+
+const jump = () => {
+    mario.classList.add('jump');
+
+    //tirando a funçao de jump pra ele realizar um loop de colocar e tirar sempre JUMP
+    setTimeout(() => {
+        mario.classList.remove('jump');
+    }, 500);
+}
+
+const loop = setInterval(() => {
+    
+    const pipePosition = pipe.offsetLeft; //verifiando o log da posição que se encontra o pipe
+    const marioPosition = +window.getComputedStyle(mario).bottom.replace('px', '');
+    
+    // SE O PIPE ESTIVER NA POSIÇAO X a animaçao dele vai parar na mesma posiçao
+    if (pipePosition <= 120 && pipePosition> 0 && marioPosition < 80) {
+
+        pipe.style.animation = 'none'
+        pipe.style.left = `${pipePosition}px`;
+
+        //animaçao do mario PARA e ele fica na mesma posiçao
+        mario.style.animation = 'none'
+        mario.style.bottom = `${marioPosition}px`;
+
+
+    }
+}, 10)
+
+
+document.addEventListener('keydown', jump);
